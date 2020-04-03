b3f953af7df554ce5af123ec1f878de8825790f9
Igor Rocha <igor.rocha@sepof.fortaleza.ce.gov.br>
2020-04-03 07:52:50 -0300

    add form

--- src/components/processo/abertura/EtapaForm/EtapaForm1/index.js
+++ src/components/processo/abertura/EtapaForm/EtapaForm1/index.js
@@ -1,30 +1,154 @@
+import { Button, Form, Input, Select } from 'antd';
+
 import { Collapse } from 'antd';
 import React from 'react';
 
+const { Option } = Select;
+const layout = {
+  labelCol: {
+    span: 8
+  },
+  wrapperCol: {
+    span: 16
+  }
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16
+  }
+};
+
 export default () => {
+  const [form] = Form.useForm();
+
+  const onGenderChange = value => {
+    switch (value) {
+      case 'male':
+        form.setFieldsValue({
+          note: 'Hi, man!'
+        });
+        break;
+
+      case 'female':
+        form.setFieldsValue({
+          note: 'Hi, lady!'
+        });
+        break;
+
+      case 'other':
+        form.setFieldsValue({
+          note: 'Hi there!'
+        });
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const onFinish = values => {
+    console.log(values);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onFill = () => {
+    form.setFieldsValue({
+      note: 'Hello world!',
+      gender: 'male'
+    });
+  };
+
   return (
     <>
-      <Collapse
-        defaultActiveKey={['1']}
-        expandIconPosition={'right'}
-        bordered={false}
-      >
-        <Collapse.Panel header={<strong>Dados Gerais</strong>} key="1">
-          <div>text</div>
-        </Collapse.Panel>
-        <Collapse.Panel header={<strong>Dados Gerais</strong>} key="2">
-          <div>text</div>
-        </Collapse.Panel>
-        <Collapse.Panel header={<strong>Descrição do Processo</strong>} key="3">
-          <div>text</div>
-        </Collapse.Panel>
-        <Collapse.Panel
-          header={<strong>Processo ou Documento Circular</strong>}
-          key="4"
+      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+        <Collapse
+          defaultActiveKey={['1']}
+          expandIconPosition={'right'}
+          bordered={false}
         >
-          <div>text</div>
-        </Collapse.Panel>
-      </Collapse>
+          <Collapse.Panel header={<strong>Dados Gerais</strong>} key="1">
+            <Form.Item
+              name="note"
+              label="Note"
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Collapse.Panel>
+          <Collapse.Panel header={<strong>Dados Gerais</strong>} key="2">
+            <Form.Item
+              name="gender"
+              label="Gender"
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+            >
+              <Select
+                placeholder="Select a option and change input text above"
+                onChange={onGenderChange}
+                allowClear
+              >
+                <Option value="male">male</Option>
+                <Option value="female">female</Option>
+                <Option value="other">other</Option>
+              </Select>
+            </Form.Item>
+          </Collapse.Panel>
+          <Collapse.Panel
+            header={<strong>Descrição do Processo</strong>}
+            key="3"
+          >
+            <Form.Item
+              noStyle
+              shouldUpdate={(prevValues, currentValues) =>
+                prevValues.gender !== currentValues.gender
+              }
+            >
+              {({ getFieldValue }) =>
+                getFieldValue('gender') === 'other' ? (
+                  <Form.Item
+                    name="customizeGender"
+                    label="Customize Gender"
+                    rules={[
+                      {
+                        required: true
+                      }
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                ) : null
+              }
+            </Form.Item>
+          </Collapse.Panel>
+          <Collapse.Panel
+            header={<strong>Processo ou Documento Circular</strong>}
+            key="4"
+          >
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+              <Button htmlType="button" onClick={onReset}>
+                Reset
+              </Button>
+              <Button type="link" htmlType="button" onClick={onFill}>
+                Fill form
+              </Button>
+            </Form.Item>
+          </Collapse.Panel>
+        </Collapse>
+      </Form>
     </>
   );
 };
